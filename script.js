13d267c27fd74c6d8fb527f3c74619b8c15769cf
Student <student@RU-102.local>
2016-01-27 14:51:51 -0700

    loops2_1

---
+++ script.js
@@ -0,0 +1,15 @@
+var animals = ['rat', 'cat', 'butterfly', 'marmot', 'ocelot']
+console.log(animals)
+// for (i =0; i<= animals.length; i++) {
+
+// }
+
+// function popFrom(Array) {
+// 	Array.pop();
+// 	return Array; 
+// }
+// testPopFrom = popFrom(animals);
+// console.log(testPopFrom);
+
+
+
